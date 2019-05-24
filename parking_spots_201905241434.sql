bda75259adc3da1c1bd4d1aa5df5fe2709d04b61
kaushalnathvani <kaushal_nathvani@yahoo.com>
2019-05-24 14:37:12 +0530

    first commit

---
+++ parking_spots_201905241434.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO public.parking_spots (available,lat,lon,address,"cost") VALUES 
+(true,22.1234131,-41.5673332,'address2',0.5)
+,(false,14.1674004,34.3456345,'address4',2)
+,(true,9.12314034,41.7865562,'address3',2)
+,(true,18.5655003,73.8050003,'address1',0.5)
+;
